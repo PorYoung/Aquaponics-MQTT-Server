feaf70168a5fae8c0c0e5aee47bd052297f7dc48
PorYoung <qc_65d94d337f5a4a93832dff41a06a8f8d@git.code.tencent.com>
2019-03-22 10:14:59 +0800

    config add

---
+++ config/index.js
@@ -0,0 +1,18 @@
+const config = {
+  server_url: 'https://localhost',
+  wechat_login_url: `https://api.weixin.qq.com/sns/jscode2session`,
+  wechat_AppID: 'wxdc501806d28dc430',
+  wechat_AppSecret: 'ba554361047bad30781920b7808ccf66',
+  MqttConfig: {
+    userPubAuth: ['instruction'],
+    devicePubAuth: ['data'],
+    userSubAuth: ['data', 'wraning'],
+    deviceSubAuth: ['instruction'],
+    publicAuth: ['test', 'info']
+  }
+}
+config.MqttConfig = Object.assign(config.MqttConfig, {
+  mqttDataAnalysisApi: config.server_url + '/api/mqttDataAnalysis',
+  separator: '#'
+})
+module.exports = config
