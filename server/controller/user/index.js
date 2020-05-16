52296bb8f1f5cb28552d7350aea2fd51bfcfa38a
PorYoung <qc_65d94d337f5a4a93832dff41a06a8f8d@git.code.tencent.com>
2020-05-16 11:10:08 +0800

    test

--- server/controller/user/index.js
+++ server/controller/user/index.js
@@ -243,7 +243,7 @@ module.exports = {
       const asyncRedisClient = asyncRedisClientConnect()
       let letter = pinyin(user.nickName, { style: pinyin.STYLE_FIRST_LETTER })[0][0].toUpperCase()[0]
       let userlist = asyncRedisClient.lrange(letter, 0, -1)
-      for (let i = 0; index < userlist.length; ++i) {
+      for (let i = 0; i < userlist.length; ++i) {
         let u = JSON.parse(userlist[i])
         console.log(u._id, user_id)
         if (u._id == user_id) {
